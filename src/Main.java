017064e2578e06600935b7ad500316eea48968c2
Таня Кишинская <lolo-20@yandex.ru>
2023-03-06 20:56:36 +0300

    calculate bonnus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double price = 13676;
+        double bonus = 20;
+        double result = price * bonus;
+        System.out.println( "Ваш бонус составляет: " + result);
+
     }
 }
