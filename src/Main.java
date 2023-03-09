cabb5e373f9a8e27b10a46a4412cd80bd30ea91d
Таня Кишинская <lolo-20@yandex.ru>
2023-03-09 23:47:38 +0300

    измение

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,17 @@ public class Main {
     public static void main(String[] args) {
         int price = 13676;
         int rubles = 20;
-        boolean isRegistered =true;
 
         int miles;
-        if(isRegistered) {
+        if(1>0) {
             miles = 1;
         } else {
             miles = 0;
         }
 
-       // int  miles= isRegistered ? 1 : 0;
+       // int  miles = (1>0) ? 1 : 0;
 
         int result = price / 20 * miles;
-
-        if (miles > 500) {
-            miles=500;
-        }
         System.out.println( "Стоимость билета: "  + price + "\nВаш бонус составляет: " + result);
 
     }
